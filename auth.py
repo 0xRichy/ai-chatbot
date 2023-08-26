14c11e7b952d73cad700e0a39a62ff6271e88b1a
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-26 22:51:19 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,12 @@
+from flask import Flask, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
 
+users = {}  # Simule une base de données d'utilisateurs
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    hashed_password = generate_password_hash(password, method='sha256')
+    users[username] = hashed_password
+    return jsonify({"message": "User registered"}), 201
