5a64b255b6063a1d6b8c1fcf420585f1151c12d8
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-26 22:51:56 +0200

    Update history.py

--- history.py
+++ history.py
@@ -1,1 +1,6 @@
-
+@app.route("/history")
+def get_history():
+    user_id = request.args.get("user_id")
+    c.execute("SELECT conversation FROM history WHERE user_id = ?", (user_id,))
+    history = c.fetchall()
+    return json.dumps(history)
