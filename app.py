2289e6ad253d9efa9ac6c038102ba3eaa2b71c9c
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-08-26 22:40:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,53 @@
-from flask import Flask, render_template, request, redirect, url_for
-import openai
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import sqlite3
+import requests
+import json
 
 app = Flask(__name__)
-openai.api_key = ''
 
-def obtenir_reponse_utilisateur(message):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=message,
-        max_tokens=50
-    )
-    return response.choices[0].text.strip()
+# Initialisation du chatbot
+chatbot = ChatBot("MyBot")
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
 
-@app.route("/", methods=["GET", "POST"])
-def chatbot():
-    if request.method == "POST":
-        openai.api_key = request.form["api_key"]
-        return redirect(url_for('chatbot'))
-    return render_template("index.html", api_key=openai.api_key)
+# Initialisation de la base de données SQLite
+conn = sqlite3.connect('chatbot.db', check_same_thread=False)
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS interactions (user_query TEXT, bot_response TEXT)''')
 
-@app.route("/chat", methods=["POST"])
-def get_chat_response():
-    user_input = request.form["user_input"]
-    chatbot_response = obtenir_reponse_utilisateur(user_input)
-    return {"response": chatbot_response}
+# Fonction pour effectuer une recherche Bing
+def bing_search(query):
+    api_key = "Votre_Clé_API"
+    url = "https://api.cognitive.microsoft.com/bing/v7.0/search"
+    headers = {"Ocp-Apim-Subscription-Key": api_key}
+    params = {"q": query, "count": 1}
+
+    response = requests.get(url, headers=headers, params=params)
+    search_results = response.json()
+
+    return search_results['webPages']['value'][0]['snippet']
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    user_query = request.args.get("msg")
+    bot_response = chatbot.get_response(user_query).text
+
+    # Sauvegarde de l'interaction dans la base de données
+    c.execute("INSERT INTO interactions VALUES (?, ?)", (user_query, bot_response))
+    conn.commit()
+
+    # Si l'utilisateur demande une recherche Bing
+    if "search for" in user_query:
+        search_query = user_query.replace("search for ", "")
+        bot_response = bing_search(search_query)
+
+    return bot_response
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run()
